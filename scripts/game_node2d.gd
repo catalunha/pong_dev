aa04784d3f4f4bcaa10b7f7a8606f097efb0825a
catalunha <catalunha@gmail.com>
2021-04-20 20:31:27 -0300

    etapa 04 - colocando BallSprite no centro do game

--- scripts/game_node2d.gd
+++ scripts/game_node2d.gd
@@ -1,16 +1,11 @@
 extends Node2D
 
+var screen_size
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
+	$BallSprite.position = screen_size*0.5
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
