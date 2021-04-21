e0367cbb38e1a848481faef71b1daead2591605c
catalunha <catalunha@gmail.com>
2021-04-20 22:14:28 -0300

    etapa 05 - Teste o movimento da bola nas 4 direções possíveis

--- scripts/game_node2d.gd
+++ scripts/game_node2d.gd
@@ -1,11 +1,18 @@
 extends Node2D
+const BALL_SPEED = 150
 
 var screen_size
+var ball_speed = BALL_SPEED
+var ball_direction
+var ball_position
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	$BallSprite.position = screen_size*0.5
+	ball_direction = Vector2(1,0)
+	ball_position = $BallSprite.position
 
 
 func _process(delta):
-	pass
+	ball_position += ball_direction * ball_speed * delta
+	$BallSprite.position = ball_position
