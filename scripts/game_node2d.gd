f9bf420f045007c98b10e872be5415842c4eb5c0
catalunha <catalunha@gmail.com>
2021-04-22 17:44:56 -0300

    etapa 06 - bola bate no topo e na base e volta ao campo

--- scripts/game_node2d.gd
+++ scripts/game_node2d.gd
@@ -9,10 +9,15 @@ var ball_position
 func _ready():
 	screen_size = get_viewport_rect().size
 	$BallSprite.position = screen_size*0.5
-	ball_direction = Vector2(1,0)
+	ball_direction = Vector2(0,-1)
 	ball_position = $BallSprite.position
 
-
 func _process(delta):
+	# A bola inicia seu movimento na direção definida no _ready()
 	ball_position += ball_direction * ball_speed * delta
+	
+	# Se a bola tocar no top ou embaixo volta ao campo
+	if(ball_position.y < 0 or ball_position.y > screen_size.y):
+		ball_direction.y = -ball_direction.y
+
 	$BallSprite.position = ball_position
